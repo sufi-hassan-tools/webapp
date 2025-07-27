26b370611e5db151b2d2aeea584b24602ee68c59
sufi-hassan-tools <122306352+sufi-hassan-tools@users.noreply.github.com>
2025-07-27 11:53:22 +0000

    Add placeholder create_store view

--- stores/views.py
+++ stores/views.py
@@ -1,6 +1,12 @@
+from django.http import HttpResponse
 from rest_framework import generics
 from .models import StoreProfile, Product
 from .serializers import StoreProfileSerializer, ProductSerializer
+
+# Placeholder view for creating a store
+def create_store(request):
+    """Temporary endpoint for creating stores."""
+    return HttpResponse("Store creation endpoint")
 
 # API Views
 class StoreProfileAPIView(generics.RetrieveAPIView):
